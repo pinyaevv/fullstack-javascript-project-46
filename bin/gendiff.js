ac4dbe9f1f3ad217e962aad3600e9e1b3ae3916a
Pinyaev Vladimir <avallakhh@gmail.com>
2024-08-23 19:00:21 +0300

    Add option and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import { program } from 'commander';
 
 program
@@ -8,12 +7,8 @@ program
   .version('1.0.0');
 
 program
-  .name('diff')
-  .description('Getting the difference between the data.')
-  .argument('<filepath1>', 'first data')
-  .argument('<filepath2>', 'second data')
-  .action((filepath1, filepath2) => {
-    console.log(`Difference between ${filepath1} and ${filepath2}`);
-  });
+  .option('-f, --format [type]', 'output format')
+  .argument(`<filepath1>`)
+  .argument(`<filepath2>`)
 
 program.parse();
