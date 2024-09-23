fff4647d9553c084ed56751d345c76a1f258b508
Pinyaev Vladimir <avallakhh@gmail.com>
2024-09-23 19:41:41 +0300

    project update 6(start plain)

--- __tests__/index.text.js
+++ __tests__/index.text.js
@@ -6,49 +6,74 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const jsonFile1 = getFixturePath('file1.json');
+const jsonFile2 = getFixturePath('file2.json');
+const ymlFile1 = getFixturePath('file1.yml');
+const ymlFile2 = getFixturePath('file2.yml');
 
-const testFileCase = [
-  {
-    file1: 'file1.json',
-    file2: 'file2.json',
-    expected: `{
-    common: {
-      - follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-    }
-}`,
-  },
+const testFileCase = `{
+  common: {
+    + follow: false
+      setting1: Value 1
+    - setting2: 200
+    - setting3: true
+    + setting3: null
+    + setting4: blah blah
+    + setting5: {
+          key5: value5
+      }
+      setting6: {
+          doge: {
+            - wow: 
+            + wow: so much
+          }
+          key: value
+        + ops: vops
+      }
+  }
+  group1: {
+    - baz: bas
+    + baz: bars
+      foo: bar
+    - nest: {
+          key: value
+      }
+    + nest: str
+  }
+- group2: {
+      abc: 12345
+      deep: {
+          id: 45
+      }
+  }
++ group3: {
+      deep: {
+          id: {
+              number: 45
+          }
+      }
+      fee: 100500
+  }
+}`;
 
-  {
-    file1: 'file1.yml',
-    file2: 'file2.yml',
-    expected: `{
-    common: {
-      - follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-    }
-}`,
-  },
-
-];
+const plainFiles = `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`;
 
 test('compare key values', () => {
-  testFileCase.forEach(({ file1, file2, expected }) => {
-    const diffFile = gendiff(getFixturePath(file1), getFixturePath(file2));
-    expect(diffFile).toEqual(expected);
-  });
+  expect(gendiff(jsonFile1, jsonFile2)).toEqual(testFileCase);
+  expect(gendiff(ymlFile1, ymlFile2)).toEqual(testFileCase);
+});
+
+test('compare key values in stylish', () => {
+  expect(gendiff(jsonFile1, jsonFile2, 'stylish')).toEqual(testFileCase);
+  expect(gendiff(ymlFile1, ymlFile2, 'stylish')).toEqual(testFileCase);
 });
